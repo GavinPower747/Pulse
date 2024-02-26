798432d1e1b79a462dbbdd5f190fc160d43f92f5
Gavin Power <gavinpower747@hotmail.com>
2024-02-26 03:04:19 +0000

    Moved auth to shared assembly

---
+++ src/Pulse.Shared/Auth/ContextExtensions.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Http;
+
+namespace Pulse.Shared.Auth;
+
+public static class ContextExtenstions
+{
+    private static readonly string UserIdClaimType =
+        "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
+
+    public static Guid GetUserId(this HttpContext context)
+    {
+        var userId = context.User.Claims.FirstOrDefault(c => c.Type == UserIdClaimType)?.Value;
+
+        return userId switch
+        {
+            null => Guid.Empty,
+            _ => Guid.Parse(userId)
+        };
+    }
+}
---
+++ src/Pulse.Shared/Auth/IdentityProvider.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Http;
+
+namespace Pulse.Shared.Auth;
+
+public class IdentityProvider(IHttpContextAccessor httpContextAccessor)
+{
+    private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
+
+    public CurrentUser GetCurrentUser() => new(_httpContextAccessor.HttpContext!);
+}
